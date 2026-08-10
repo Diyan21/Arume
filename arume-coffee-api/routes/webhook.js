3e40662ca7a8f61bbc60594aae5ad6ae5ce4b96f
Diyan21 <diyanaxl@gmail.com>
2026-08-10 12:21:37 +0700

    Delete arume-coffee-api directory

--- arume-coffee-api/routes/webhook.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { handleCallback } from '../controllers/paymentController.js';
-
-const router = Router();
-
-// POST /api/webhook/duitku - Alias for Duitku payment callback
-router.post('/duitku', handleCallback);
-
-export default router;
